417e29884783791e5fb914ce33241e251be3285a
joacland <joachim.landstrom@gmail.com>
2021-12-28 00:27:04 +0100

    Finished the file that reads Eikon the OrganizationID and attaches them to Compustat's gvkey

--- src/gvkey_orgid/gvkey_orgid_v2.py
+++ src/gvkey_orgid/gvkey_orgid_v2.py
@@ -16,15 +16,30 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import pandas as pd
 
+pd.set_option("display.max_columns", None)
+pd.set_option("display.expand_frame_repr", False)
+pd.set_option("max_colwidth", None)
 
 # DEFINITIONS
-def read_csv_file(in_file):
+def read_csv_file(
+    in_file,
+    delimiter="\t",
+    dtype="str",
+    na_values=" ",
+    low_memory=False,
+    **kwargs,
+):
     """Read in_file as csv file with tab delimiter. Space is treated as NaN.
 
-    Assumes all variables are string variables
+    Assumes as default that all variables are string variables and that the
+    file is tab-separated
     """
     df = pd.read_csv(
-        in_file, delimiter="\t", dtype=str, na_values=" ", low_memory=False
+        in_file,
+        delimiter=delimiter,
+        dtype=dtype,
+        na_values=na_values,
+        low_memory=low_memory,
     )
     return df
 
@@ -54,26 +69,36 @@ def create_out_file(out_file, out_header):
     return
 
 
-def save_to_csv_file(in_dta, out_file):
+def save_to_csv_file(
+    in_dta,
+    out_file,
+    mode="a",
+    sep="\t",
+    encoding="utf-8",
+    index=False,
+    header=False,
+    **kwargs,
+):
     """Saves the data frame in_dta as out_file.
 
-    out_file is tab separated and saving use 'append' and enconding is
-    'utf-8'.
+    out_file is as default tab separated and saving use 'append' as default
+    with default enconding 'utf-8'.
     """
 
     in_dta.to_csv(
         out_file,
-        mode="a",
-        sep="\t",
-        encoding="utf-8",
-        index=False,
-        header=False,
+        mode=mode,
+        sep=sep,
+        encoding=encoding,
+        index=index,
+        header=header,
+        **kwargs,
     )
     # print('Saved ' + str(len(in_dta)) + ' lines to file ' + str(out_file))
     return
 
 
-def save_to_parquet_file(df, file, compression, **kwargs):  # TODO Add kwarg
+def save_to_parquet_file(df, file, compression="snappy", **kwargs):
     """
     Enter three arguments and save a dataframe as an Apache Parquet database.
 
@@ -86,17 +111,42 @@ def save_to_parquet_file(df, file, compression, **kwargs):  # TODO Add kwarg
     compression: The database's compression. Available compressions are
     ‘snappy’, ‘gzip’, ‘brotli’, or none.
 
-    Return: Returns an Apache Parquet database with a speficic compression.
+    Return: Returns an Apache Parquet database with a specific compression.
 
     Notes:
-    **kwargs not yet implemented.
     The function requires either 'pyarrow', or 'fastparquet'.
     """
     # Drop existing file
     if pl.Path.exists(file):
         pl.Path.unlink(file)
     # Save DB
-    df.to_parquet(file, compression=compression)
+    df.to_parquet(file, compression=compression, **kwargs)
+
+
+def delisted_ric(in_df):
+    """Find RICs delisted, strip delisting suffix, and append to original"""
+    delisted = in_df[in_df.ric.str.contains("\^[A-Z][0-9]{2}")]
+    delisted_original = delisted["ric"].str.replace(
+        r"\^[A-Z][0-9]{2}", "", regex=True, case=True
+    )
+    delisted_original = delisted_original.to_frame()
+    delisted_original = delisted_original.drop_duplicates()
+    df = in_df.append(delisted_original)
+    df = df.drop_duplicates()
+    df = df.dropna(how="any")
+    return df
+
+
+def ipo_ric(in_df):
+    """Find IPO RICs and strip the IPO- prefix, and append to original"""
+    ipo = in_df[in_df.ric.str.contains("^IPO-")]
+    ipo_original = ipo["ric"].str.replace(r"^IPO-", "", regex=True, case=True)
+    ipo_original = ipo_original.to_frame()
+    ipo_original = ipo_original.drop_duplicates()
+    df = in_df.append(ipo_original)
+    df = df.drop_duplicates()
+    df = df.dropna(how="any")
+    return df
 
 
 # PATHS
@@ -119,36 +169,49 @@ if __name__ == "__main__":
     """
     # Load manually collected relations between OrganizationID and gvkey
     gvkey_orgid_manual = read_csv_file(
-        pl.Path.joinpath(raw_path, "gvkey_orgid_manual.csv")
+        pl.Path.joinpath(raw_path, "gvkey_organizationid_manual.csv")
     )
     gvkey_orgid_manual = gvkey_orgid_manual[["organizationid", "gvkey"]]
 
-    # Read Eikon's data
+    # Read Eikon's data and add OrganizationID to ISIN, SEDOL, CUSIP
     print("Reading the Eikon files")
-    eikon_files = ["ISIN", "SEDOL", "CUSIP"]
+    eikon_files = ["organizationid", "isin", "sedol", "cusip"]
     # eikon_files = [ISIN', 'SEDOL', 'CUSIP', 'RIC']
     dict_of_df = {}
     for file in eikon_files:
         key_name = file.lower()
-        name = key_name + "_orgid.csv"
+        name = f"ric_{key_name}.csv"
         dict_of_df[key_name] = read_csv_file(pl.Path.joinpath(raw_path, name))
         dict_of_df[key_name].columns = dict_of_df[key_name].columns.str.lower()
         # print(str(key_name) + ' ' + str(len(dict_of_df[key_name])))
 
-    isin = dict_of_df["isin"]
-    isin = isin.drop_duplicates()
+    organizationid = dict_of_df["organizationid"]
+    organizationid = ipo_ric(organizationid)
+    organizationid = delisted_ric(organizationid)
 
-    sedol = dict_of_df["sedol"]
-    sedol = sedol.drop_duplicates()
+    isin_eikon = dict_of_df["isin"]
+    isin_eikon = ipo_ric(isin_eikon)
+    isin_eikon = delisted_ric(isin_eikon)
+    isin_eikon = pd.merge(isin_eikon, organizationid, how="inner", on=["ric"])
+    isin_eikon = isin_eikon.drop("ric", axis=1)
+    isin_eikon = isin_eikon.drop_duplicates()
 
-    cusip = dict_of_df["cusip"]
-    cusip = cusip.drop_duplicates()
+    sedol_eikon = dict_of_df["sedol"]
+    sedol_eikon = ipo_ric(sedol_eikon)
+    sedol_eikon = delisted_ric(sedol_eikon)
+    sedol_eikon = pd.merge(sedol_eikon, organizationid, how="inner", on=["ric"])
+    sedol_eikon = sedol_eikon.drop("ric", axis=1)
+    sedol_eikon = sedol_eikon.drop_duplicates()
 
-    organizationid_all = isin["organizationid"]
-    organizationid_all = organizationid_all.append(
-        [sedol["organizationid"], cusip["organizationid"]]
-    )
-    organizationid_all = organizationid_all.drop_duplicates()
+    cusip_eikon = dict_of_df["cusip"]
+    cusip_eikon = ipo_ric(cusip_eikon)
+    cusip_eikon = delisted_ric(cusip_eikon)
+    cusip_eikon = pd.merge(cusip_eikon, organizationid, how="inner", on=["ric"])
+    cusip_eikon = cusip_eikon.drop("ric", axis=1)
+    cusip_eikon = cusip_eikon.drop_duplicates()
+
+    organizationid_unique = organizationid["organizationid"]
+    organizationid_unique = organizationid_unique.drop_duplicates()
 
     # ric = dict_of_df['ric']
     # ric = ric.drop_duplicates()
@@ -205,16 +268,16 @@ if __name__ == "__main__":
     gvkey_all = gvkey_all.drop_duplicates()  # All gvkey in Compustat
 
     # Merge Eikon and Compustat, keep the pairs 'gvkey' and 'ric'
-    print("Merging Eikon and Compustat")
-    isin_df = pd.merge(isin, isin_stata, how="inner", on=["isin"])
+    print("Connect Eikon and Compustat firm identification keys")
+    isin_df = pd.merge(isin_eikon, isin_stata, how="inner", on=["isin"])
     isin_df = isin_df[["organizationid", "gvkey"]]
     isin_df = isin_df.drop_duplicates()
 
-    sedol_df = pd.merge(sedol, sedol_stata, how="inner", on=["sedol"])
+    sedol_df = pd.merge(sedol_eikon, sedol_stata, how="inner", on=["sedol"])
     sedol_df = sedol_df[["organizationid", "gvkey"]]
     sedol_df = sedol_df.drop_duplicates()
 
-    cusip_df = pd.merge(cusip, cusip_stata, how="inner", on=["cusip"])
+    cusip_df = pd.merge(cusip_eikon, cusip_stata, how="inner", on=["cusip"])
     cusip_df = cusip_df[["organizationid", "gvkey"]]
     cusip_df = cusip_df.drop_duplicates()
 
@@ -260,16 +323,17 @@ if __name__ == "__main__":
     gvkey_swe = gvkey_swe[gvkey_swe["fic"] == "SWE"]
     gvkey_swe = gvkey_swe.dropna(how="all", subset=["cusip", "sedol", "isin"])
 
-    print(len(gvkey_miss))
-    r_gvkey = len(gvkey_miss) / len(gvkey_modern)
-    print(r_gvkey)
-    print(len(gvkey_swe))
+    # print(len(gvkey_miss))
+    # r_gvkey = len(gvkey_miss) / len(gvkey_modern)
+    # print(r_gvkey)
+    # print(len(gvkey_swe))
+    # print(gvkey_swe)
 
     # Identify missing OrganizationID
     organizationid_match = organizationid_gvkey_df["organizationid"]
     organizationid_match = organizationid_match.drop_duplicates()
 
-    r_gvkey = len(organizationid_all) - len(organizationid_match)
+    r_gvkey = len(organizationid_unique) - len(organizationid_match)
     # print(r_gvkey)
     # r_gvkey = len(organizationid_match) / len(organizationid_all)
     # print(r_gvkey)
@@ -295,6 +359,11 @@ if __name__ == "__main__":
     create_out_file(pl.Path.joinpath(out_path, "cusip_stata.csv"), my_header)
     save_to_csv_file(cusip_stata, pl.Path.joinpath(out_path, "cusip_stata.csv"))
 
+    organizationid_gvkey_df = organizationid_gvkey_df.rename(
+        columns={
+            "organizationid": "OrganizationID",
+        }
+    )
     my_header = list(organizationid_gvkey_df.columns.values)
     create_out_file(
         pl.Path.joinpath(out_path, "gvkey_organizationid.csv"), my_header
@@ -313,9 +382,23 @@ if __name__ == "__main__":
         },
         version=119,
     )
-    save_to_parquet_file(organizationid_gvkey_df, pl.Path.joinpath(out_path, "gvkey_organizationid.parquet.brotli"), compression="brotli")
-    print("File " + "--" + str(pl.Path.joinpath(out_path, "gvkey_organizationid.parquet.brotli")) + "--" + " is saved.")
-    print("It has " + str(len(organizationid_gvkey_df)) + " rows, and has the following header:")
+    save_to_parquet_file(
+        organizationid_gvkey_df,
+        pl.Path.joinpath(out_path, "gvkey_organizationid.parquet.brotli"),
+        compression="brotli",
+    )
+    print(
+        "File "
+        + "--"
+        + str(pl.Path.joinpath(out_path, "gvkey_organizationid.parquet.brotli"))
+        + "--"
+        + " is saved."
+    )
+    print(
+        "It has "
+        + str(len(organizationid_gvkey_df))
+        + " rows, and has the following header:"
+    )
     print(my_header)
 
     # File below outputs rows with missing organizationid
